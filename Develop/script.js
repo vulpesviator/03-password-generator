c9679faa95b466932805b48493c1956c32e0df99
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-04-15 14:12:30 -0500

    Adds the starter code and closes #1

---
+++ Develop/script.js
@@ -0,0 +1,20 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+function generatePassword() {
+  
+}
+
+console.log(password);
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
