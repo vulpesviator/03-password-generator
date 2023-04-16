15c5ecd1a921d9c32af567b6a34132c5ac7d61e1
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-04-16 15:28:02 -0500

    Added prompts for character selection

--- Develop/script.js
+++ Develop/script.js
@@ -2,39 +2,31 @@
 var generateBtn = document.querySelector("#generate");
 
 var appendix = {
-  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  lowerCase: "abcdefghijklmnopqrstuvwxyz",
-  numbers: "0123456789",
-  // symbols: " !"#$%&'()*+,-./:;<=>?@[]^_`{|}~"
+  upperCase: `ABCDEFGHIJKLMNOPQRSTUVWXYZ`,
+  lowerCase: `abcdefghijklmnopqrstuvwxyz`,
+  numbers: `0123456789`,
+  special: ` !\"#$%&\'()*+,-./:;<=>?@[]^_\`{|}~`
 };
+
+
 
 /* WHEN I select GENERATE PASSWORD there is a prompt to determine LENGTH of the PASSWORD that is at least 8 and no more than 128 characters
 THEN there is a prompt to confirm if I should include LOWERCASE, UPPERCASE, NUMERIC, and/or SPECIAL CHARACTERS  */
 
 function generatePassword() {
   var length = window.prompt(`Input a length for your password between "8" and "128"`),
-      charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+      appendix = { upperCase: confirm(`Would you like uppercase letters?`),
+                   lowerCase: confirm(`Would you like lower case letters?`),
+                   numbers: confirm(`Would you like to use numbers?`),
+                   special: confirm(`Would you like to use special characters?`)},
       finalPassword = "";
-      for (var i = 0, n = charset.length; i < length; i++) {
-        finalPassword += charset.charAt(Math.floor(Math.random() * n));
+      for (var i = 0, n = appendix.length; i < length; i++) {
+        finalPassword += appendix[Math.floor(Math.random() * n)];
       return finalPassword;
       }
   
   
-  /*
-  var generatePassword = "";
-  var passwordLength = window.prompt("Input a length between 8 and 128");
 
-  if (passwordLength >= 8 && passwordLength <= 128) {
-    var totalPassword = passwordLength * (appendix.numbers[Math.floor(Math.random() * appendix.length)]);
-    console.log(totalPassword);
-  } else {
-    window.alert("Please choose a number between 8 and 128.");
-  }
-  console.log(passwordLength);
-  var finalPassword = generatePassword.slice(0, length);
-  return finalPassword;
-  */
 };
 
 // 
