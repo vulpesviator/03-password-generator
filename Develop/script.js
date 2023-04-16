ecff3bd0adc0861465316046443d65538c855ca6
travdoeswhat <123843930+vulpesviator@users.noreply.github.com>
2023-04-16 17:18:06 -0500

    FIrst pass on generate logic

--- Develop/script.js
+++ Develop/script.js
@@ -2,46 +2,60 @@
 var generateBtn = document.querySelector("#generate");
 
 var appendix = {
-  upperCase: `ABCDEFGHIJKLMNOPQRSTUVWXYZ`,
   lowerCase: `abcdefghijklmnopqrstuvwxyz`,
+  upperCase: `ABCDEFGHIJKLMNOPQRSTUVWXYZ`,
   numbers: `0123456789`,
   special: ` !\"#$%&\'()*+,-./:;<=>?@[]^_\`{|}~`
 };
 
-
-
-/* WHEN I select GENERATE PASSWORD there is a prompt to determine LENGTH of the PASSWORD that is at least 8 and no more than 128 characters
-THEN there is a prompt to confirm if I should include LOWERCASE, UPPERCASE, NUMERIC, and/or SPECIAL CHARACTERS  */
-
 function generatePassword() {
+  /* WHEN I select GENERATE PASSWORD there is a prompt to determine LENGTH of the PASSWORD that is at least 8 and no more than 128 characters */
   var passwordLength = window.prompt(`Please enter the length for your password between "8" and "128"`);
   
-  if (passwordLength <= 8 || passwordLength >= 128) {
+  if (passwordLength < 9 || passwordLength > 129) {
     window.alert(`The value has to be the length of your password betwen the values of "8" and "128". Please try again.`);
-    passwordLength = window.prompt(`Please enter the length of your password between "8" and "128"`)
+    passwordLength = window.prompt(`Please enter the length of your password between "8" and "128"`);
   }
-  
+
+  var finalPassword = "";
+
+  for (let i = 0; i < length; i++) {
+    finalPassword += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+
+  /* THEN there are prompts to confirm if I should include LOWERCASE, UPPERCASE, NUMERIC, and/or SPECIAL CHARACTERS  */
+  var lowercase = true;
   var uppercase = window.confirm(`Would you like to use uppercase letters?`);
-  var lowercase = window.confirm(`Would you like to use lowercase letters?`);
   var numbers = window.confirm(`Would you like to use numbers?`);
-  var special = window.confirm(`Would you like to use special characters?`)
+  var special = window.confirm(`Would you like to use special characters?`);
+
+  var charSet = "";
+    // if (lowercase) {
+    //   charSet = appendix.lowerCase;
+    // } else if (uppercase) {
+    //   charSet = appendix.uppercase;
+    // } else if (numbers) {
+    //   charSet = appendix.numbers;
+    // } else if (special) {
+    //   charSet = appendix.special;
+    // } else if (lowercase && uppercase) {
+    //   charSet = appendix.lowercase + appendix.uppercase;
+    // }
+    
+    
+    // else {
+    //   window.alert(`Please choose at least one `)
+    // }
   
+  console.log(charSet);
   console.log(length);
   console.log(uppercase);
   console.log(lowercase);
   console.log(numbers);
   console.log(special);
   
-  var finalPassword = "";
   return finalPassword;
-  /*
   
-      for (var i = 0, n = appendix.length; i < appendix.length; i++) {
-        finalPassword = appendix.charAt[Math.floor(Math.random() * n)];
-      return finalPassword;
-      }
-  */
-
 };
 
 console.log(Number(length));
@@ -61,11 +75,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-/* { upperCase: confirm(`Would you like uppercase letters?`),
-                   lowerCase: confirm(`Would you like lower case letters?`),
-                   numbers: confirm(`Would you like to use numbers?`),
-                   special: confirm(`Would you like to use special characters?`)}
-                   */
